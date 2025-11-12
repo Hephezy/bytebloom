08bbd23751d73abef06a750f87458fa71c045301
Oluwapelumi Martins <111850401+Hephezy@users.noreply.github.com>
2025-11-12 12:44:07 +0100

    Fixed migration issues

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,9 +29,9 @@ async function startServer() {
     })
   );
 
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}/graphql`);
+  const PORT = Number(process.env.PORT) || 4000;
+  app.listen(PORT, "0.0.0.0", () => {
+    console.log(`🚀 Server ready at http://0.0.0.0:${PORT}/graphql`);
   });
 }
 
